b2bd08493a03f2fc7cc5c574ade52b94b21b4f1e
dpataki92 <ptamasdaniel92@gmail.com>
2021-09-16 19:07:55 +0200

    add examples for type annotation usage

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,4 +1,4 @@
-const apples: number = 5;
+const apples = 5; // if declaration and initialization are on the same line, TS infers the type
 let speed: string = 'fast';
 let hasName: boolean = true;
 
@@ -28,3 +28,30 @@ let point: {x: number; y: number; }  = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Function that returns 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: {x: number, y: number} = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20}
+
+// 2) When we declare a variable on one line and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variabble whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAbvoveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAbvoveZero = numbers[i];
+  }
+}
+
