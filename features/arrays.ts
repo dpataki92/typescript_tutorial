32871ac9d44defb2ed58803135a27e3eb6488e2f
dpataki92 <ptamasdaniel92@gmail.com>
2021-10-05 21:09:45 +0200

    add examples for array in typescript

---
+++ features/arrays.ts
@@ -0,0 +1,27 @@
+const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+const dates =[new Date(), new Date()];
+
+const carsByMake = [
+  ['f150'],
+  ['corolla'],
+  ['camaro']
+];
+
+const carsByMake2: string[][] = [] // it will be an array of array of strings
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+// Flexible types
+const importantDates = [new Date(), '2030-10-10'];
+const importanDates2: (Date | string)[] = [];
+importantDates.push(200);
