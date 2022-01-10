22ca378afdbe1be20175e92b168f3be397ff29ba
dpataki92 <ptamasdaniel92@gmail.com>
2022-01-10 18:04:10 +0100

    add initial rts project

---
+++ rts/src/props/Child.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface ChildProps {
+  color: string;
+  onClick: () => void;
+}
+
+export const Child = ({ color, onClick }: ChildProps) => {
+  return <div>
+    {color}
+    <button onClick={onClick}>Click me</button>
+  </div>;
+};
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick }: ChildProps) => {
+  return <div>
+    {color}
+    <button onClick={onClick}>Click me</button>
+  </div>;
+};
