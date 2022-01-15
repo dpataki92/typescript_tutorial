ddbdec39350eccf2b26199e1c802faa8e0f5abd6
dpataki92 <ptamasdaniel92@gmail.com>
2022-01-15 15:28:53 +0100

    apply different Action interfaces

--- redux-ts/src/state/reducers/repositoriesReucer.ts
+++ redux-ts/src/state/reducers/repositoriesReucer.ts
@@ -4,20 +4,35 @@ interface RepositoriesState {
   data: string[];
 }
 
-interface Action {
-  type: string;
-  payload?: any;
+interface SearchRepositoriesAction {
+  type: "search_repositories";
 }
 
-const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
+interface SearchRepositoriesSuccessAction {
+  type: "search_repositories_success";
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: "search_repositories_error";
+  payload: string;
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action:
+    | SearchRepositoriesAction
+    | SearchRepositoriesSuccessAction
+    | SearchRepositoriesErrorAction
+): RepositoriesState => {
   switch (action.type) {
-    case 'search_repositories':
+    case "search_repositories":
       return { loading: true, error: null, data: [] };
-    case 'search_repositories_success':
+    case "search_repositories_success":
       return { loading: false, error: null, data: action.payload };
-    case 'search_repositories_error':
+    case "search_repositories_error":
       return { loading: false, error: action.payload, data: [] };
-    default: 
+    default:
       return state;
   }
 };
