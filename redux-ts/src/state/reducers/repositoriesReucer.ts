aace81e9ec7700941f5bc42830ee290c03af35d1
dpataki92 <ptamasdaniel92@gmail.com>
2022-01-15 15:15:20 +0100

    add extra checks to reducer

--- redux-ts/src/state/reducers/repositoriesReucer.ts
+++ redux-ts/src/state/reducers/repositoriesReucer.ts
@@ -4,14 +4,19 @@ interface RepositoriesState {
   data: string[];
 }
 
-const reducer = (state: RepositoriesState, action: any) => {
+interface Action {
+  type: string;
+  payload?: any;
+}
+
+const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
   switch (action.type) {
     case 'search_repositories':
       return { loading: true, error: null, data: [] };
     case 'search_repositories_success':
       return { loading: false, error: null, data: action.payload };
     case 'search_repositories_error':
-      return { loading: false, error: true, data: [] };
+      return { loading: false, error: action.payload, data: [] };
     default: 
       return state;
   }
